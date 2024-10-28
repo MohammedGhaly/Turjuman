1d1384a4eb51c78236240de28c733ae071b4db6a
MohammedGhaly <medooghaly7@gmail.com>
2024-10-28 21:57:03 +0300

    initial commit

---
+++ extension/README.md
@@ -0,0 +1,1 @@
+# Turujman Chrome Extension
---
+++ frontEnd/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-6xl text-gray-200 h-screen flex items-center justify-center font-semibold">
+      Welcome to Turjuman
+    </div>
+  );
+}
+
+export default App;
